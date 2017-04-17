e083d1d7df766b259d8dfed3f62547fa63554eab
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2017-04-17 17:18:07 +0530

    hey new push

--- MailX/src/gui.java
+++ MailX/src/gui.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 
 
 public class gui {
-	/// commit by samarth
+	/// new push by samarth
 	JFrame j = new JFrame("Mailx");
 	JLabel from,pass,to,sub,message;
 	JButton send,clear,quit;
