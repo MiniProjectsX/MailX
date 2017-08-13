c05479da134303e5e65445405c1b99e8f11eeb95
Samarth Halyal <samarthhalyal@gmail.com>
2017-08-13 15:08:24 +0530

    Update Controller.java

--- MailX/src/Controller.java
+++ MailX/src/Controller.java
@@ -1,14 +1,21 @@
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimeMessage.RecipientType;
 import javax.swing.JOptionPane;
 
 public class Controller {
-	Controller(String user,String host,String password,String to2,String sub,String message){
+
+	Controller(String user,String host,String password,String to2,String sub,String message,String n,String fp){
 		Properties prop = new Properties();
 		prop.put("mail.smtp.host", host);
 		prop.put("mail.smtp.user", user);
@@ -22,16 +29,30 @@ public class Controller {
 			Session session = Session.getDefaultInstance(prop);
 			
 			/// Configure the message
-			MimeMessage message2 = new MimeMessage(session);
-			message2.addRecipient(RecipientType.TO, new InternetAddress(to2));
-			message2.setFrom(new InternetAddress(user));
-			message2.setSubject(sub);
-			message2.setText(message);
+			MimeMessage message1 = new MimeMessage(session);
+			message1.addRecipient(RecipientType.TO, new InternetAddress(to2));
+			message1.setFrom(new InternetAddress(user));
+			message1.setSubject(sub);
+			message1.setText(message);
 			
+			// File sending part
+			if(n != ""){
+				MimeBodyPart messageBodyPart = new MimeBodyPart();
+		        Multipart multipart = new MimeMultipart();    
+		        messageBodyPart = new MimeBodyPart();
+		        String file = fp;
+		        String fileName = n;
+		        DataSource source = new FileDataSource(file);
+		        messageBodyPart.setDataHandler(new DataHandler(source));
+		        messageBodyPart.setFileName(fileName);
+		        multipart.addBodyPart(messageBodyPart);
+		        message1.setContent(multipart);
+			}
+	        
 			/// Create a Transport
 			Transport transport = session.getTransport("smtp");
 			transport.connect(host,user,password);
-			transport.sendMessage(message2, message2.getAllRecipients());
+			transport.sendMessage(message1, message1.getAllRecipients());
 			transport.close();
 			JOptionPane.showMessageDialog(null, "Mail Sent", "", JOptionPane.INFORMATION_MESSAGE);
 		}catch(Exception e1){
@@ -39,3 +60,4 @@ public class Controller {
 		}
 	}
 }
+
